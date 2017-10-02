2f68cda1e7c5c0398dbe8bac8cbd16f903ced58c
vamshi-indla <vamshi.indla@gmail.com>
2017-10-01 20:25:42 -0400

    Python3.0 updated

---
+++ stockdemo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 15:21:44 2017
+
+@author: vamshi294
+"""
+
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+import lstm, time #helper libraries
+#%%
